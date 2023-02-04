2de256df9f4ed0bd27ab4558821acba37404bf2b
Noah-Chappell <noahmchappell@gmail.com>
2023-02-04 15:20:51 -0800

    fixed issue where defenitions would be deleted if not adj instead of full element

--- dataParsers/defEmotionSlimmer.py
+++ dataParsers/defEmotionSlimmer.py
@@ -14,12 +14,15 @@ del data["unadded"]
 
 colorMap = {"anger":"red", "disgust":"green", "joy":"yellow", "surprise":"orange", "fear":"black", "sadness":"blue"}
 
+toDelete = []
 for word in data:
     if (data[word]["definitions"] == []):
-        del data[word]["definitions"]
+        toDelete.append(word)
     else:
         data[word]["color"] = colorMap[data[word]["emotion"]]
 
+for word in toDelete:
+    del data[word]
 
 jsonFile = open("./jsonFiles/defEmotionMapSlimmed.json", "w")
 try:
