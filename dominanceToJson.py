25eee5218a5d9c92d09859edd91f8ce4d4318d03
Noah-Chappell <noahmchappell@gmail.com>
2023-02-04 12:38:59 -0800

    created a basic script to parse through NRC-VAD-Lexicon and return a json with each word associated with its intensity value

---
+++ dominanceToJson.py
@@ -0,0 +1,19 @@
+import json
+
+filepath = "NRC-VAD-Lexicon.txt"
+
+file = open(filepath, "r")
+
+dominanceMap = {}
+try:
+    fileLines = file.readlines()
+except Exception as e:
+    print (e)
+    file.close()        
+
+
+for line in fileLines:
+    #each space between data is a special Large Space character
+    dominanceMap[line[:-19]] = float(line[-5:].strip())
+
+json.dump(dominanceMap, open("dominanceMap.json", "w"))
