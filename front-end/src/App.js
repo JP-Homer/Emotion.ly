4090a008254b20d89242a819f6cd56242168eee1
Mykyta Medvediev <arlekin8881@gmail.com>
2023-02-04 13:48:12 -0800

    Initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Flex,
+  Text,
+  TextArea,
+  Button,
+  Slider,
+  SliderTrack,
+  SliderFileldTrack,
+  SliderThumb,
+  SliderMark,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Flex>
+      <Text>Hello, world!</Text>
+    </Flex>
+  );
+}
+
+export default App;
