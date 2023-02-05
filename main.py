baaf9290898ff2aa4e7d562157d35629d6ec672c
patino209 <patinoc1@uci.edu>
2023-02-04 18:41:11 -0800

    Modified main flask app

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def index():
-    return jsonify("This works.")
-
-@app.route("/new")
-def new():
-    return jsonify("new page.")
+@app.route("/", methods=['POST'])
+def handle_json():
+    string = request # string from front end
+    # do something with string
+    # return json
