291c69aaea234ca06bfa790534d46629041c529b
patino209 <patinoc1@uci.edu>
2023-02-04 18:50:54 -0800

    Testing post request

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify, request
+import json
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def handle_json():
-    return "works" # string from front end
+@app.route("/",methods=['POST'])
+def handle_json(): # string from front end
+     data = request.json
+     string = data.get('sentence')
+     print(string)
+
     # do something with string
     # return json
