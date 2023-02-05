8fef452c2669ae2205b24de28767208cc2de273b
patino209 <patinoc1@uci.edu>
2023-02-04 19:44:47 -0800

    Error handling, commented code

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from flask import Flask, jsonify, request
-import functionsNeeded
-import json
 from flask_cors import CORS
+from functionsNeeded import get_adjective_info
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/", methods=['POST'])
+# Parses information and returns json file to front end
 def handle_json():
-    data = json.loads(request.data)
-    string = data['sentence']
-    print(string)
-    obj = functionsNeeded.get_adjective_info(str(string), 2)
-    print(obj)
-    return jsonify(obj)
+    data = json.loads(request.data) # Receives json given by frontend
+    string = data['sentence'] # Grabs the sentence from the json
+    obj = get_adjective_info(str(string), 2) # Runs backend algorithm on string
+
+    if obj:
+        return jsonify(obj) # returns a json file with information required by front end
+
+    # Returns 400 code if json file is empty (no adjectives with emotional connotation in sentence)
+    return "No adjective that denotes emotion was found. Please try another sentence", 400
 
 
 if __name__ == "__main__":
