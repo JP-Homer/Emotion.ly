35cc6ceda3a0b14731bd1272bb8631a05711b334
Noah-Chappell <noahmchappell@gmail.com>
2023-02-04 18:09:30 -0800

    created function that given a word and radius will return the words of that emotion that surround it in ranking

--- functionsNeeded.py
+++ functionsNeeded.py
@@ -10,20 +10,89 @@ import json
     # words()
     #     word[definition]
 
-
-
-def return_surroundings(word : str, radius : int) -> list:
-    filepath = "./jsonFiles/defEmotionMapSlimmed.json"
+def return_surrounding_words(index : int, emotion : str, radius : int) -> list[list, list]:
+    '''
+    given an index and an emotion will return words that suround that index in ranking of that emotion
+    '''
+    filepath = "./jsonFiles/rankedEmotionsSublist.json"
     file = open(filepath, "r")
 
     try:
-        wordData = json.load(file)
+        rankedData = json.load(file)
     except Exception as e:
         print(e)
     finally:
         file.close()
     
+    if (index - radius < 0):
+        radius += -1*(index - radius)
+    elif (index + radius > len(rankedData[emotion])):
+        radius += index + radius - len(rankedData[emotion])
+
+
+
+    leftwords = []
+    for i in range(index - radius, index):
+        if (i >= 0):
+            leftwords.append(rankedData[emotion][i])
+    rightwords = []
+    for i in range(index + 1, index + radius + 1):
+        if (i < len(rankedData[emotion])):
+            rightwords.append(rankedData[emotion][i])
     
+    return [leftwords, rightwords]
+
+def find_surroundings(word : str, radius : int) -> list:
+    '''
+    given a word and radius will return the words of that emotion that surround it in ranking
+    '''
+    filepath = "./jsonFiles/rankedEmotionsSublistObjects.json"
+    file = open(filepath, "r")
+
+    try:
+        rankedData = json.load(file)
+    except Exception as e:
+        print(e)
+    finally:
+        file.close()
+    
+    returnData = {word : {}}
+
+    for emotion in rankedData:
+        if (word in rankedData[emotion]):
+            wordData = rankedData[emotion][word]
+            
+            returnData[word]["base"] = wordData["emotion"]
+            returnData[word]["color"] = wordData["color"]
+
+            left_words, right_words = return_surrounding_words(wordData["index"], wordData["emotion"], radius)
+            difference = wordData["index"]
+            if (len(left_words) != 0):
+                difference = rankedData[emotion][left_words[0]]["index"]
+            
+            returnData[word]["rank"] = wordData["index"] - difference
+
+            returnData[word]["words"] = []
+            for lword in left_words:
+                wordDict = {"word" : lword}
+                wordDict["definition"] = rankedData[emotion][lword]["definitions"]
+                wordDict["rank"] = rankedData[emotion][lword]["index"] - difference
+
+                returnData[word]["words"].append(wordDict)
+            
+            for rword in right_words:
+                wordDict = {"word" : rword}
+                wordDict["definition"] = rankedData[emotion][rword]["definitions"]
+                wordDict["rank"] = rankedData[emotion][rword]["index"] - difference
+
+                returnData[word]["words"].append(wordDict)
+    
+    return returnData
+            
+
+
+    
+
 
 
 
@@ -72,4 +141,5 @@ def find_adjective(sentance : str) ->list:
 
 if __name__ == "__main__":
     # print(find_adjective("hello, there HOW'RE you you abhorrent man with a happy, ugly walk"))
+    print (find_surroundings("dun", 5))
     pass
